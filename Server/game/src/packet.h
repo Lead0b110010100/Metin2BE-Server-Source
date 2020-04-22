4c5b2df696c4eec27a65ba824ad7b94f6f4a74e3
Lead0b110010100 <Lead0b110010100@outlook.com>
2020-04-22 08:14:36 +0200

    Added slot marketing system.

--- Server/game/src/packet.h
+++ Server/game/src/packet.h
@@ -1302,6 +1302,9 @@ struct packet_exchange
 	DWORD	arg1;	// vnum
 	TItemPos	arg2;	// cell
 	DWORD	arg3;	// count
+#ifdef WJ_ENABLE_TRADABLE_ICON
+	TItemPos	arg4;	// srccell
+#endif
 	long	alSockets[ITEM_SOCKET_MAX_NUM];
 	TPlayerItemAttribute aAttr[ITEM_ATTRIBUTE_MAX_NUM];
 };
